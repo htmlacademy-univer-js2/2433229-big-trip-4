d20514e64ae17124bcca9fe4d470d758fe2b3182
MaxSiryatov <siryatov.max@mail.ru>
2024-04-11 21:14:27 +0500

    build configuring

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path'); 
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js', 
+
+  output: {
+    filename: 'bundle.js', 
+    path: path.resolve(__dirname, "build"),
+    clean: true,
+  },
+  devtool: "source-map",
+  
+  module: {
+    rules: [
+      {
+        test: /\.css$/, 
+        use: ['style-loader', 'css-loader'], 
+      },
+    ],
+  },
+  
+  plugins: [
+    new CopyPlugin({
+        patterns: [
+          {
+            from: 'public',
+            globOptions: {
+              ignore: ['**/index.html'],
+            },
+          },
+        ],
+    }),
+  ],
+};
