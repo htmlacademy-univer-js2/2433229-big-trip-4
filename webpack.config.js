9c8c040cb319901780edcb4d720c6054fb7a5286
MaxSiryatov <siryatov.max@mail.ru>
2024-04-11 22:04:19 +0500

    transpiler configuring

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,18 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/, 
-        use: ['style-loader', 'css-loader'], 
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
       },
     ],
   },
